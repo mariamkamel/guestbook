d7b4c9025e88701e6c01e6eb599f7aee190a7d2a
mariamkamel <mariammkamel98@gmail.com>
2020-09-23 22:32:03 +0200

    login ui added

--- guestbook-fe/src/containers/Login/Login.jsx
+++ guestbook-fe/src/containers/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import { useHistory } from "react-router-dom";
-import axios from 'axios'
+import { useHistory, Link } from "react-router-dom";
 import  loginUser  from '../../auth'
 
 function Login() {
@@ -13,10 +12,11 @@ function Login() {
 
    async function onSubmit (event) {
         event.preventDefault();
-    const res = await loginUser(
+        const res = await loginUser(
             {email: state.email, 
             password: state.password
         })
+        
      if(res) history.push('/comments')
     }        
     
@@ -31,15 +31,32 @@ function Login() {
 
      const { email, password } = state
         return (
-            <div >
-                <form style={{border: "solid"}} onSubmit={onSubmit}>
-                    <label>Email</label> <br/>
-                    <input type="text" id="email" name="email" value={email} onChange={onChange} placeholder="email"/> <br/>
-                    <label>Password</label> <br/>
-                    <input type="password" id="password" name="password" value={password} onChange={onChange}/> <br/>
-                    <button type="submit" >Log in</button>
-                 </form>
-            </div>
+            <div className=" flex justify-center content-center  ">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
+                        Email
+                    </label>
+                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="email" type="text" placeholder="Email"  value={email} onChange={onChange}/>
+                 </div>
+                 <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="password" type="password" placeholder="Password"value={password} onChange={onChange} />
+                 </div>
+                 <div className="flex justify-center ">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Sign In
+                    </button>
+                 </div>
+                 <p className="text-center text-gray-500 text-xs">
+                   Don't have an account? <br/>
+                   <Link className="text-blue-500 hover:text-blue-800" to="/register">Sign up here</Link>
+                </p>
+            </form>
+        </div> 
+ 
         );
     
 }
