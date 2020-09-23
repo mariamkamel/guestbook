f985225181db4d7c15dc8587628a1dd911d7b9d1
mariamkamel <mariammkamel98@gmail.com>
2020-09-23 23:43:59 +0200

    login/register ui updates

--- guestbook-fe/src/containers/Login/Login.jsx
+++ guestbook-fe/src/containers/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { useHistory, Link } from "react-router-dom";
 import  loginUser  from '../../auth'
-
 function Login() {
     let history = useHistory();
     const [state , setState] = useState({
@@ -31,30 +30,37 @@ function Login() {
 
      const { email, password } = state
         return (
-            <div className=" flex justify-center content-center  ">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
-                        Email
-                    </label>
-                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="email" type="text" placeholder="Email"  value={email} onChange={onChange}/>
-                 </div>
-                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Password
-                    </label>
-                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="password" type="password" placeholder="Password"value={password} onChange={onChange} />
-                 </div>
-                 <div className="flex justify-center ">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Sign In
-                    </button>
-                 </div>
-                 <p className="text-center text-gray-500 text-xs">
-                   Don't have an account? <br/>
-                   <Link className="text-blue-500 hover:text-blue-800" to="/register">Sign up here</Link>
-                </p>
+            <div className=" flex justify-center content-center bg-blue-800 h-screen ">
+              <div className="flex items-center content-center">
+                  <form className=" bg-white shadow-2xl  rounded px-8 pt-6 pb-8 mb-4" style={{"height": "fit-content"}} onSubmit={onSubmit}>
+               
+                        <div  className=" flex justify-center font-mono font-bold content-center">
+                        <span className="text-center text-blue-800 text-2xl">Login</span>
+                        </div>
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
+                                Email
+                            </label>
+                            <input className="border rounded w-full py-2 px-3 text-gray-700 " id="email" type="text" placeholder="Email"  value={email} onChange={onChange}/>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="border rounded w-full py-2 px-3 text-gray-700 " id="password" type="password" placeholder="Password"value={password} onChange={onChange} />
+                        </div>
+                        <div className="flex justify-center mb-2 ">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                Sign In
+                            </button>
+                        </div>
+                        <p className="text-center text-gray-500 text-xs">
+                        Don't have an account? <br/>
+                        <Link className="text-blue-500 hover:text-blue-800" to="/register">Sign up here</Link>
+                        </p>
             </form>
+            </div>
         </div> 
  
         );
--- guestbook-fe/src/containers/Register/Register.jsx
+++ guestbook-fe/src/containers/Register/Register.jsx
@@ -38,36 +38,42 @@ function Register() {
 
      const { email, password, username } = state
         return (
-            <div className=" flex justify-center content-center  ">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
-            <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
-                        Email
-                    </label>
-                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="email" type="text" placeholder="Email"  value={email} onChange={onChange}/>
-                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username" >
-                        Username
-                    </label>
-                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="username" type="text" placeholder="Username"  value={username} onChange={onChange}/>
-                 </div>
-                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Password
-                    </label>
-                    <input className="border rounded w-full py-2 px-3 text-gray-700 " id="password" type="password" placeholder="Password" value={password} onChange={onChange} />
-                 </div>
-                 <div className="flex justify-center ">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                       Register
-                    </button>
-                 </div>
-                 <p className="text-center text-gray-500 text-xs">
-                 
-                   <Link className="text-blue-500 hover:text-blue-800" to="/login">Login</Link>
-                </p>
-            </form>
+            <div className="flex justify-center content-center bg-blue-800 h-screen ">
+                <div className="flex items-center content-center">
+
+                 <form className="bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+                  <div  className=" flex justify-center font-mono font-bold content-center">
+                        <span className="text-center text-blue-800 text-2xl">Register</span>
+                  </div>  
+                  <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
+                            Email
+                        </label>
+                        <input className="border rounded w-full py-2 px-3 text-gray-700 " id="email" type="text" placeholder="Email"  value={email} onChange={onChange}/>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username" >
+                            Username
+                        </label>
+                        <input className="border rounded w-full py-2 px-3 text-gray-700 " id="username" type="text" placeholder="Username"  value={username} onChange={onChange}/>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input className="border rounded w-full py-2 px-3 text-gray-700 " id="password" type="password" placeholder="Password" value={password} onChange={onChange} />
+                    </div>
+                    <div className="flex justify-center mb-2">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Register
+                        </button>
+                    </div>
+                    <p className="text-center text-gray-500 text-xs">
+                    
+                    <Link className="text-blue-500 hover:text-blue-800" to="/login">Login</Link>
+                    </p>
+                </form>
+             </div>
         </div> 
         );
     
